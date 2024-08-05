e60b13d14054bee8f0e3d258663d849bf9de8424
bmbany <59551445+bmbany@users.noreply.github.com>
2024-08-05 11:19:14 +0900

    TextRenderer 기능 추가
    텍스트 사이사이 특정 구간만 색 바꾸는 기능 추가

--- Engine/Codes/TextRenderer.cpp
+++ Engine/Codes/TextRenderer.cpp
@@ -3,18 +3,15 @@
 
 using namespace Engine;
 
-Engine::TextRenderer::TextRenderer(const char* name)
-	: Component(name)
-{
-}
-
-void TextRenderer::Start()
-{
+Engine::TextRenderer::TextRenderer(const char* name, D2D1::ColorF color, float size, DWRITE_FONT_WEIGHT fontWeight)
+	: Component(name), _color(color), _fontSize(size), _fontWeight(fontWeight)
+{	
 	GraphicManager* pGraphicMgr = GraphicManager::GetInstance();
 	_pSolidColorBrush = pGraphicMgr->GetSolidColorBrush();
 	_pDeviceContext = pGraphicMgr->GetDeviceContext();
+	_pWriteFactory = pGraphicMgr->GetWriteFactory();
 
-	pGraphicMgr->GetWriteFactory()->CreateTextFormat(
+	_pWriteFactory->CreateTextFormat(
 		L"¸¼Àº °íµñ", NULL,
 		_fontWeight, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL,
 		_fontSize, L"", &_pWriteTextFormat);
@@ -27,15 +24,44 @@ void TextRenderer::Start()
 void TextRenderer::Render()
 {
 	if (nullptr == _pDeviceContext)
-		return;
+		return;	
 
 	_pDeviceContext->SetTransform(D2D1::Matrix3x2F::Identity());
-	_pSolidColorBrush->SetColor(D2D1::ColorF(_color));
+	_pSolidColorBrush->SetColor(_color);
 	_pSolidColorBrush->SetOpacity(1.f);
-	_pDeviceContext->DrawText(_text, lstrlen(_text), _pWriteTextFormat, _drawRect, _pSolidColorBrush);
+
+	if (nullptr != _pWriteTextLayout)
+	{
+		_pDeviceContext->DrawTextLayout(D2D1::Point2F(0.f, 0.f), _pWriteTextLayout, _pSolidColorBrush);
+	}
+	else
+	{		
+		_pDeviceContext->DrawText(_text, lstrlen(_text), _pWriteTextFormat, _drawRect, _pSolidColorBrush);
+	}
+
+}
+
+void Engine::TextRenderer::SetTextLayout(const wchar_t* text, float width, float height)
+{
+	_pWriteFactory->CreateTextLayout(text, lstrlen(text), _pWriteTextFormat, width, height, &_pWriteTextLayout);
+}
+
+void Engine::TextRenderer::SetTextRange(unsigned int start, unsigned int length, D2D1::ColorF color)
+{	
+	if (nullptr == _pWriteTextLayout)
+		return;
+	ID2D1SolidColorBrush* pSolidColorBrush = nullptr;
+
+	_pDeviceContext->CreateSolidColorBrush(color, &pSolidColorBrush);
+	DWRITE_TEXT_RANGE textRange = { start, length };
+	_pWriteTextLayout->SetDrawingEffect(pSolidColorBrush, textRange);
+	_colorBrushs.push_back(pSolidColorBrush);
 }
 
 void TextRenderer::Free()
 {
+	for (auto& brush : _colorBrushs)
+		SafeRelease(brush);
+
 	SafeRelease(_pWriteTextFormat);
 }
--- Engine/Headers/TextRenderer.h
+++ Engine/Headers/TextRenderer.h
@@ -6,34 +6,35 @@ namespace Engine
 	class TextRenderer : public Engine::Component
 	{
 	public:
-		explicit TextRenderer(const char* name);
+		explicit TextRenderer(const char* name, D2D1::ColorF color = D2D1::ColorF::White, float size = 25.f, DWRITE_FONT_WEIGHT fontWeight = DWRITE_FONT_WEIGHT_NORMAL);
 	private:
 		virtual ~TextRenderer() = default;
 
 	public:
-		void Start() override;
 		void Render() override;
 
-	public:
-		void SetFontColor(D2D1::ColorF::Enum color) { _color = color; }
-		void SetFontSize(float size) { _fontSize = size; }
+	public:		
 		void SetText(const wchar_t* text) { _text = text; }
 		void SetDrawRect(const D2D1_RECT_F& drawRect) { _drawRect = drawRect; }
-		void SetFontWeight(DWRITE_FONT_WEIGHT fontWeight) { _fontWeight = fontWeight; }
+		void SetTextLayout(const wchar_t* text, float width, float height);
+		void SetTextRange(unsigned int start, unsigned int length, D2D1::ColorF color);
 
 	private:
 		// Component을(를) 통해 상속됨
 		void Free() override;
 
 	private:
-		D2D1_RECT_F				_drawRect;
-		IDWriteTextFormat*		_pWriteTextFormat = nullptr;
-		ID2D1SolidColorBrush*	_pSolidColorBrush = nullptr;
-		ID2D1DeviceContext*		_pDeviceContext = nullptr;
+		std::list<ID2D1SolidColorBrush*>	_colorBrushs;
+		D2D1_RECT_F							_drawRect{};
+		IDWriteTextFormat*					_pWriteTextFormat = nullptr;
+		ID2D1SolidColorBrush*				_pSolidColorBrush = nullptr;
+		ID2D1DeviceContext*					_pDeviceContext = nullptr;
+		IDWriteTextLayout*					_pWriteTextLayout = nullptr;
+		IDWriteFactory*						_pWriteFactory = nullptr;
 
-		const wchar_t*			_text = nullptr;
-		float					_fontSize = 20.f;		
-		D2D1::ColorF::Enum      _color = D2D1::ColorF::White;
-		DWRITE_FONT_WEIGHT		_fontWeight = DWRITE_FONT_WEIGHT_NORMAL;
+		const wchar_t*						_text = nullptr;
+		float								_fontSize = 20.f;		
+		D2D1::ColorF						_color = D2D1::ColorF::White;
+		DWRITE_FONT_WEIGHT					_fontWeight = DWRITE_FONT_WEIGHT_NORMAL;
 	};
 }
