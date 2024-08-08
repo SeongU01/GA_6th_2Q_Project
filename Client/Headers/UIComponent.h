5ff76854650b4b9a948de9b2950c3a00531038c4
SUIN <87413903+lsuinl@users.noreply.github.com>
2024-08-08 12:13:10 +0900

    uicreate 함수 추가

--- Client/Headers/UIComponent.h
+++ Client/Headers/UIComponent.h
@@ -11,6 +11,17 @@ protected:
 protected:
 	void AddUI(const UI::UIInfo& info);
 	void SetDontDestroyObject(bool isActive);
+	UI::UIInfo CreateInfo(const wchar_t* name, const wchar_t* tag, int fixframe, Vector3 position, Vector3 scale, Engine::Transform* parent)
+	{
+		UI::UIInfo info;
+		info.name = name;
+		info.textureTag = tag;
+		info.fixFrame = fixframe;
+		info.position = position;
+		info.scale = scale;
+		info.pParent = parent;
+		return info;
+	}
 
 private:
 	std::list<UI*> _UIs;
