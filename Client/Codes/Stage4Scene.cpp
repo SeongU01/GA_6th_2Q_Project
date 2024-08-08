edd2c4c5806b77a930daeb58b6e5433300f02d60
SeongU <blacknaco85@gmail.com>
2024-08-08 20:35:33 +0900

    conflict resolution

--- Client/Codes/Stage4Scene.cpp
+++ Client/Codes/Stage4Scene.cpp
@@ -35,15 +35,15 @@ bool Stage4Scene::Initialize()
     ((int)LayerGroup::Tile, L"Tile",
         Map::Create(Vector3(stage1.width, stage1.height, 0.f), Vector3(stage1.tileOffsetX, stage1.tileOffsetY, 0.f)
           ,Vector3(WINCX>>1,WINCY>>1,0.f),stage1.mapOffsetY));*/
-    Engine::AddObjectInLayer((int)LayerGroup::Tile, L"Tile", Map::Create(stage1, Vector3(WINCX >> 1, WINCY >> 1, 0.f)));
+    //Engine::AddObjectInLayer((int)LayerGroup::Tile, L"Tile", Map::Create(stage1, Vector3(WINCX >> 1, WINCY >> 1, 0.f)));
 
-    Engine::AddObjectInLayer((int)LayerGroup::Player, L"Player", TestPlayer::Create());
-    Engine::AddObjectInLayer((int)LayerGroup::Object, L"Mountain1", Obstacle::Create(std::pair(Vector3(7.f, 0.f, 0.f), Vector3(8.f, 0.f, 0.f)), L"Obstacle_Mountain"));
-    Engine::AddObjectInLayer((int)LayerGroup::Object, L"water", Obstacle::Create(std::pair(Vector3(6.f, 1.f, 0.f), Vector3(8.f, 3.f, 0.f))));
-    TestEnemy* monster = TestEnemy::Create(); Engine::AddObjectInLayer((int)LayerGroup::Enemy, L"Enemy", monster); monster->GetComponent<Enemy>()->SetGridPosition({ 0,1,0 });
+    ////Engine::AddObjectInLayer((int)LayerGroup::Player, L"Player", TestPlayer::Create());
+    //Engine::AddObjectInLayer((int)LayerGroup::Object, L"Mountain1", Obstacle::Create(std::pair(Vector3(7.f, 0.f, 0.f), Vector3(8.f, 0.f, 0.f)), L"Obstacle_Mountain"));
+    //Engine::AddObjectInLayer((int)LayerGroup::Object, L"water", Obstacle::Create(std::pair(Vector3(6.f, 1.f, 0.f), Vector3(8.f, 3.f, 0.f))));
+    //TestEnemy* monster = TestEnemy::Create(); Engine::AddObjectInLayer((int)LayerGroup::Enemy, L"Enemy", monster); monster->GetComponent<Enemy>()->SetGridPosition({ 0,1,0 });
 
-    _pCardManagement = CardManagement::GetInstance();
-    _pCardManagement->LoadCard((path + L"Data/Card").c_str());
+    //_pCardManagement = CardManagement::GetInstance();
+    //_pCardManagement->LoadCard((path + L"Data/Card").c_str());
 
 
     Engine::GameObject* pBackGround = Engine::GameObject::Create();
