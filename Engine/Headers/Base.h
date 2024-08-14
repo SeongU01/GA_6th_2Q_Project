3af6a5061f70d4ab2491606ebf2a5326807c76ab
bmbany <bmbany@naver.com>
2024-08-14 14:31:33 +0900

    Update Base.h

--- Engine/Headers/Base.h
+++ Engine/Headers/Base.h
@@ -18,16 +18,16 @@ namespace Engine
 
 		inline void SetActive(bool isActive) { _isActive = isActive; }
 		inline void SetName(const wchar_t* name) { _name = name; }
-		inline const wchar_t* GetName() const { return _name.c_str(); }
+		inline const wchar_t* GetName() const { return _name; }
 		inline bool IsActive() const { return _isActive; }
-		inline bool operator==(const wchar_t* str) { return _name == str; }
-		inline bool operator!=(const wchar_t* str) { return _name != str; }
+		inline bool operator==(const wchar_t* str) { return !lstrcmp(_name, str); }
+		inline bool operator!=(const wchar_t* str) { return lstrcmp(_name, str); }
 
 	protected:
 		virtual void Free() = 0;
 
 	private:
-		std::wstring	_name;
+		const wchar_t*	_name;
 		bool			_isActive = true;
 	};
 }
